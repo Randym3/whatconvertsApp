539c2ccd9cc49a7e13dd88c0a05b6ba30718957e
Randy <menesesrandy@gmail.com>
2022-04-27 22:50:53 -0700

    create salesforce lead in wehook CREATE trigger

--- server.js
+++ server.js
@@ -10,68 +10,76 @@ app.use(express.json())
 app.use(cors());
 
 
-const transporter = nodemailer.createTransport({
-  port: 465,               // true for 465, false for other ports
-  host: "smtp.gmail.com",
-     auth: {
-          user: 'menesesrandy33@gmail.com',
-          pass: 'Saxxy333',
-       },
-  secure: true,
-  });
+// const transporter = nodemailer.createTransport({
+//   port: 465,               // true for 465, false for other ports
+//   host: "smtp.gmail.com",
+//      auth: {
+//           user: 'menesesrandy33@gmail.com',
+//           pass: 'Saxxy333',
+//        },
+//   secure: true,
+//   });
   
+//   const mailData = {
+//     from: 'support@randym3.com',  // sender address
+//       to: 'menesesrandy@gmail.com',   // list of receivers
+//       subject: 'Sending Email using Node.js',
+//       text: 'That was easy!',
+//       html: `leadName: ${lead.Name}`,
+//     };
 
-
+// transporter.sendMail(mailData, function (err, info) {
+//     if(err)
+//     console.log(err)
+//     else
+//     console.log(info);
+// });
 
 app.all('/', function(req, res){
     console.log("connected");
     res.sendFile(__dirname + '/views/form.html');
 });
-    
-app.get("/test-jsforce", function(req, res){
 
-    // console.log(salesforceConn);
-    salesforceConn.sobject("Lead").retrieve("00Q8b00001vWDOREA4", function(err, lead) {
-        if (err) { return console.error(err); }
-        console.log("Name : " + lead.Name);
-
-        const mailData = {
-            from: 'support@randym3.com',  // sender address
-              to: 'menesesrandy@gmail.com',   // list of receivers
-              subject: 'Sending Email using Node.js',
-              text: 'That was easy!',
-              html: `leadName: ${lead.Name}`,
-            };
-        
-        transporter.sendMail(mailData, function (err, info) {
-            if(err)
-            console.log(err)
-            else
-            console.log(info);
-        });
-        
-        res.json(lead);
-        // ...
-      });
-      
-})
 app.post('/webhook/whatconverts/create', function(req, res){
-    console.log(req.body.lead_id);
+    let whatconvertsLead = req.body;
+    console.log(`lead received (id: ${whatconvertsLead.lead_id}), now send to salesforce`);
+    // "additional_fields": {
+    //     "SalesForce": "Pending",
+    //     "Company Name": "finally",
+    //     "Email": "new@gmail.com",
+    //     "Message": "this worked",
+    //     "Name": "RANDY NEW TEST"
+    //   },
+
+    let leadDetails = {
+        LastName: whatconvertsLead.additional_fields['Name'],
+        Company: whatconvertsLead.additional_fields['Company Name'],
+        Email: whatconvertsLead.additional_fields['Email'],
+        Description: whatconvertsLead.additional_fields['Message'],
+        whatconverts_lead_id: whatconvertsLead.lead_id
+    }
+    salesforceConn.sobject("Lead").create(leadDetails, function(err, ret) {
+        if (err || !ret.success) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+        console.log("Created record id : " + ret.id);
+        return res.json(ret);
+    });
     res.json(200);
 });
 
 app.get('/webhook/whatconverts/update', function(req, res){
-    res.json("nice!");
     
-    // axios.get('https://app.whatconverts.com/api/v1/leads/71952990', {
+    // axios.get('https://app.whatconverts.com/api/v1/leads/72142024', {
     //     auth: {
     //         username: WHATCONVERTS_API_TOKEN,
     //         password: WHATCONVERTS_API_SECRET
     //     }
     // }).then(data => {
     //     console.log(data.data);
+    //     res.json(data.data);
     // }).catch(err=>console.log(err))
-    
 });
     
 app.listen(PORT, () => {
